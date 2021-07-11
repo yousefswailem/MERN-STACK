fcf8b573e0026b2e8959a424adc0883a3f2e980f
yousef <ronorozoroz@hotmail.com>
2021-07-11 20:30:02 +0300

    Ninja

--- OOP/Ninja.js
+++ OOP/Ninja.js
@@ -13,14 +13,34 @@ class Ninja{
     showStats(){
         console.log(`${this.constructor.name} his speed ${this.speed} his health ${this.health} his strength ${this.strength}`);
     }
-    drinkSake(health){
+    drinkSake(){
         this.health+=10;
         
     }
 }
 const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.drinkSake();
-ninja1.drinkSake();
-ninja1.drinkSake();
-ninja1.showStats();
+// ninja1.sayName();
+// ninja1.drinkSake();
+// ninja1.drinkSake();
+// ninja1.drinkSake();
+// ninja1.showStats();
+
+class Sensei extends Ninja{
+     constructor(name){
+        super(name);
+         this.wisdom=10;
+         this.speed = 10;
+         this.strength =10;
+         this.health = 200;
+     }
+     speakWisdom(){
+        const child= super.drinkSake();
+         console.log(`here you can get 10 hp`);
+     }
+     drinkSake(){
+         console.log(`new drinkSake`);
+     }
+}
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
