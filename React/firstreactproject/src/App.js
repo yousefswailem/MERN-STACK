c434122513cc3986edb8693714d245c005315c21
yousef <ronorozoroz@hotmail.com>
2021-07-12 20:23:09 +0300

    FirstReactProject

---
+++ React/firstreactproject/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+        <h1>Hello Dojo!</h1>
+        <h2>Things i need to do: </h2>
+        <ul>
+          <li>learn react</li>
+          <li>Climb mt.Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
